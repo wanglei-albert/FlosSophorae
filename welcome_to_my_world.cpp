204dd57e6fac68a53a58325d2ce21e3a55521920
wanglei-albert <82852097+wanglei-albert@users.noreply.github.com>
2021-08-05 13:12:10 +0800

    create a new file
    This file is an brief introduction, more things will be done later.

---
+++ welcome_to_my_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    using namespace std;
+    cout << "Welcome to my repository-FlosSophorae! "
+         << "The inspiration of this name comes from the beautiful locust trees growing everywhere in my hometown. " << endl
+         << "And the flower is not only wonderful but also eaten in many ways. Evey May, the world is almost fully" << endl 
+         << "decorated by the flower, and every breath is fragrant. It's really amazing." << endl;
+         
+    return 0;  // the return is not necessary as the compiler will automatically do it for you.
+}
